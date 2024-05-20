1404d86aa38f66c13f225ab92eeaba35fb97bb0d
Eric Ferreira <eriic1002@gmail.com>
2024-05-20 20:18:14 +0200

    Some install bug solved

--- install.sh
+++ install.sh
@@ -12,17 +12,16 @@ grayColour="\e[0;37m\033[1m"
 
 echo -e "${yellowColour}[!] Updating system...${endColour}"
 sleep 1
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get upgrade
 
 echo -e "${yellowColour}[!] Installing firsts packets...${endColour}"
 sleep 1
 sudo apt install build-essential git vim libxcb-util0-dev libxcb-ewmh-dev \
     libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev \
-    libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev xclip locate i3lock-fancy -y
-
-sudo apt install libxcb-randr0-dev libxcb-xtest0-dev libxcb-xinerama0-dev \
-    libxcb-shape0-dev libxcb-xkb-dev libpcre3-dev -y
+    libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev xclip locate i3lock-fancy \
+    open-vm-tools-desktop open-vm-tools libxcb-randr0-dev libxcb-xtest0-dev libxcb-xinerama0-dev \
+    libxcb-shape0-dev libxcb-xkb-dev libpcre3-dev libglib2.0-bin -y
 
 echo -e "${yellowColour}[!] Installing i3lock-fancy...${endColour}"
 sleep 1
@@ -31,48 +30,49 @@ cd i3lock-fancy
 sudo make install
 cd ..
 
+USER=$(whoami)
 echo -e "${yellowColour}[!] Installing bspwm...${endColour}"
 sleep 1
 sudo apt install bspwm -y
-mkdir "~/.config/bspwm"
-cp ./bspwm/bspwmrc "~/.config/bspwm/"
-chmod +x ~/.config/bspwm/bspwmrc
-mkdir "~/.config/bspwm/scripts"
-cp ./bspwm/bspwm_resize "~/.config/bspwm/scripts"
-chmod +x ~/.config/bspwm/scripts/bspwm_resize
-cp ./bspwm/ethernet_status.sh "~/.config/bspwm/scripts"
-chmod +x ~/.config/bspwm/scripts/ethernet_status.sh
-cp ./bspwm/target_ip_status.sh "~/.config/bspwm/scripts"
-chmod +x ~/.config/bspwm/scripts/target_ip_status.sh
-cp ./bspwm/vpn_status.sh "~/.config/bspwm/scripts"
-chmod +x ~/.config/bspwm/scripts/vpn_status.sh
-touch "~/.config/bspwm/scripts/target_ip"
+mkdir "/home/$USER/.config"
+mkdir "/home/$USER/.config/bspwm"
+cp ./bspwm/bspwmrc "/home/$USER/.config/bspwm/"
+chmod +x /home/$USER/.config/bspwm/bspwmrc
+mkdir "/home/$USER/.config/bspwm/scripts"
+cp ./bspwm/bspwm_resize "/home/$USER/.config/bspwm/scripts"
+chmod +x /home/$USER/.config/bspwm/scripts/bspwm_resize
+cp ./bspwm/ethernet_status.sh "/home/$USER/.config/bspwm/scripts"
+chmod +x /home/$USER/.config/bspwm/scripts/ethernet_status.sh
+cp ./bspwm/target_ip_status.sh "/home/$USER/.config/bspwm/scripts"
+chmod +x /home/$USER/.config/bspwm/scripts/target_ip_status.sh
+cp ./bspwm/vpn_status.sh "/home/$USER/.config/bspwm/scripts"
+chmod +x /home/$USER/.config/bspwm/scripts/vpn_status.sh
+touch "/home/$USER/.config/bspwm/scripts/target_ip"
 
 echo -e "${yellowColour}[!]${endColour} Installing sxhkd..."
 sleep 1
 sudo apt install sxhkd -y
-mkdir "~/.config/sxhkd"
-cp ./sxhkd/sxhkdrc "~/.config/sxhkd/"
+mkdir "/home/$USER/.config/sxhkd"
+cp ./sxhkd/sxhkdrc "/home/$USER/.config/sxhkd/"
 
 echo -e "${yellowColour}[!] Installing gnome-terminal...${endColour}"
 sleep 1
 sudo apt install gnome-terminal dconf-cli -y
 sudo git clone --depth 1 https://github.com/junegunn/fzf.git /root/.fzf
 sudo /root/.fzf/install
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+git clone --depth 1 https://github.com/junegunn/fzf.git /home/$USER/.fzf
+/home/$USER/.fzf/install
 
 gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
-cp gnome-terminal/gtk.css ~/.config/gtk-3.0/
+cp gnome-terminal/gtk.css /home/$USER/.config/gtk-3.0/
 dconf load /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ < gnome-terminal/profile.dconf
 gsettings set org.gnome.Terminal.ProfilesList default 'b1dcc9dd-5262-4d8d-a863-c897e6d979b9'
 
 echo -e "${yellowColour}[!] Installing polybar...${endColour}"
 sleep 1
 sudo apt install polybar -y
-mkdir ~/.config/polybar
-cp -r ./polybar/* ~/.config/polybar/
-git clone 
+mkdir /home/$USER/.config/polybar
+cp -r ./polybar/* /home/$USER/.config/polybar/
 
 
 echo -e "${yellowColour}[!] Configuring lightdm...${endColour}"
@@ -89,23 +89,23 @@ sudo apt install libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev \
     libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-glx0-dev \
     libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev \
     libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev \
-    libxext-dev meson ninja-build uthash-dev -y
+    libxext-dev meson ninja-build uthash-dev cmake -y
 
-git clone https://github.com/yshui/picom
-cd picom; meson setup --buildtype=release build
+git clone https://github.com/yshui/picom ./picom-installer
+cd picom-installer/; meson setup --buildtype=release build
 ninja -C build
 ninja -C build install
 cd ..
-mkdir "~/.config/picom"
-cp ./picom/picom.conf ~/.config/picom/
+mkdir "/home/$USER/.config/picom"
+cp ./picom/picom.conf /home/$USER/.config/picom/
 
 echo -e "${yellowColour}[!] Installing rofi...${endColour}"
 sleep 1
 sudo apt install rofi -y
-mkdir ~/.config/rofi
-mkdir ~/.config/rofi/themes
-cp ./rofi/rounded-common.rasi ~/.config/rofi/themes/
-cp ./rofi/rounded-custom.rasi ~/.config/rofi/themes/
+mkdir /home/$USER/.config/rofi
+mkdir /home/$USER/.config/rofi/themes
+cp ./rofi/rounded-common.rasi /home/$USER/.config/rofi/themes/
+cp ./rofi/rounded-custom.rasi /home/$USER/.config/rofi/themes/
 
 
 echo -e "${yellowColour}[!] Installing Hack Nerd Fonts...${endColour}"
@@ -115,9 +115,9 @@ sudo cp ./fonts/* /usr/local/share/fonts/
 echo -e "${yellowColour}[!] Installing zsh & powerlevel10k...${endColour}"
 sleep 1
 sudo apt install zsh zsh-autosuggestions zsh-syntax-highlighting -y
-cp ./zsh/.zshrc "~/"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-cp ./zsh/.p10k.zsh "~/"
+cp ./zsh/.zshrc "/home/$USER/"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/$USER/powerlevel10k
+cp ./zsh/.p10k.zsh "/home/$USER/"
 sudo rm /root/.zshrc
 USER=$(whoami)
 sudo ln /home/$USER/.zshrc /root/.zshrc
@@ -130,12 +130,12 @@ sudo cp ./zsh/main-highlighter.zsh /usr/share/zsh-syntax-highlighting/highlighte
 sudo mkdir /usr/share/zsh-sudo
 sudo wget -P /usr/share/zsh-sudo https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh
 sudo apt install bat lsd
-cp ./zsh/.inputrc ~/
+cp ./zsh/.inputrc /home/$USER/
 
 echo -e "${yellowColour}[!] Installing feh...${endColour}"
 sleep 1
 sudo apt install feh -y
-mkdir "~/.config/wallpapers"
+mkdir "/home/$USER/.config/wallpapers"
 sudo mkdir "/usr/share/wallpapers/custom"
 sudo cp "wallpaper.jpeg" "/usr/share/wallpapers/custom/wallpaper.jpg"
 
@@ -148,13 +148,13 @@ fc-cache -v
 
 echo -e "${yellowColour}[!] Configuring Burpsuite...${endColour}"
 sleep 1
-cat ~/.BurpSuite/UserConfigCommunity.json | sed 's/Light/Dark/; s/11/13/g' > tmp.tmp
-cat tmp.tmp > ~/.BurpSuite/UserConfigCommunity.json
+cat /home/$USER/.BurpSuite/UserConfigCommunity.json | sed 's/Light/Dark/; s/11/13/g' > tmp.tmp
+cat tmp.tmp > /home/$USER/.BurpSuite/UserConfigCommunity.json
 rm tmp.tmp
 
 echo -e "${yellowColour}[!] Installing Nvim with Nvchad...${endColour}"
 sleep 1
-git clone https://github.com/NvChad/starter ~/.config/nvim
+git clone https://github.com/NvChad/starter /home/$USER/.config/nvim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
@@ -166,9 +166,9 @@ vim.cmd([[
   hi SignColumn guibg=NONE ctermbg=NONE
   hi NormalNC guibg=NONE ctermbg=NONE
   hi EndOfBuffer guibg=NONE ctermbg=NONE
-]])' >> ~/.config/nvim/init.lua
-cp ./nvim/chadrc.lua ~/.config/nvim/lua/chadrc.lua
-cp ./nvim/init.lua ~/.local/share/nvim/lazy/NvChad/lua/nvchad/plugins/init.lua
+]])' >> /home/$USER/.config/nvim/init.lua
+cp ./nvim/chadrc.lua /home/$USER/.config/nvim/lua/chadrc.lua
+cp ./nvim/init.lua /home/$USER/.local/share/nvim/lazy/NvChad/lua/nvchad/plugins/init.lua
 
 sudo cp -r /home/$USER/.config/nvim /root/.config/
 sudo cp ./nvim/chadrc.lua /root/.config/nvim/lua/chadrc.lua
